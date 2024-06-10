b6cd0ecffc1274f7dd8a35cc21723e4d4deecf0b
Carlos  Trujillo Neyra <carlos.trujillo@alticsa.com>
2024-06-09 21:23:14 -0500

    feat: Add DBZ module and main page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
 
   // En tu "imports" se importan los módulos
-  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
